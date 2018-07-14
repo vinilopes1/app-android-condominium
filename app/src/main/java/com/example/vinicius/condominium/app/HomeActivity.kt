f707db29f1aff8dafef0b188c8fb66d62c1cd9d4
vinilopes1 <lopesvinicius1707@gmail.com>
2018-07-14 04:16:13 -0300

    Lógica de Tabs e Drawer adicionadas.

--- app/src/main/java/com/example/vinicius/condominium/app/HomeActivity.kt
+++ app/src/main/java/com/example/vinicius/condominium/app/HomeActivity.kt
@@ -1,13 +1,119 @@
 package com.example.vinicius.condominium.app
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.view.GravityCompat
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.example.vinicius.condominium.R
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_bar_home.*
+import kotlinx.android.synthetic.main.content_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        val fragmentAdapter = TabsPager(supportFragmentManager)
+        viewpager.adapter=fragmentAdapter
+        tab_layout.setupWithViewPager(viewpager)
+
+        //initComponents()
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
     }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun initComponents():Unit{
+
+        var tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        var viewPager: ViewPager = findViewById(R.id.viewpager)
+        var tabsPager  = TabsPager(supportFragmentManager)
+
+
+        viewPager.adapter(tabsPager)
+        tabLayout.setupWithViewPager(viewPager)
+
+
+    }
+
+
 }
+
+private operator fun PagerAdapter?.invoke(tabsPager: TabsPager) {
+}
+
+
---
+++ app/src/main/java/com/example/vinicius/condominium/app/TabsPager.kt
@@ -0,0 +1,43 @@
+package com.example.vinicius.condominium.app
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class TabsPager: FragmentStatePagerAdapter{
+
+    var titles: Array<String> = arrayOf("Home","Avisos","Pesquisa")
+
+    constructor(fm:FragmentManager): super(fm)
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        when(position) {
+            0 -> return "Home"
+            1 -> return "Avisos"
+            else
+            -> return "Pesquisa"
+        }
+    }
+
+    override fun getItem(position: Int): Fragment? {
+
+        var homeFragment = HomeFragment()
+        var avisoFragment = AvisoFragment()
+        var pesquisaFragment = PesquisaFragment()
+
+        when(position){
+            0 -> return homeFragment
+            1 -> return avisoFragment
+            2 -> return pesquisaFragment
+        }
+
+        return null
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+
+}
