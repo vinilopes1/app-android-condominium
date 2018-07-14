0c442261960bd62f9a6641c97be8edd8e78bd5fb
vinilopes1 <lopesvinicius1707@gmail.com>
2018-07-14 07:54:53 -0300

    Adição de imagens, substiuição de texto por ícones na tab.

--- app/src/main/java/com/example/vinicius/condominium/app/HomeActivity.kt
+++ app/src/main/java/com/example/vinicius/condominium/app/HomeActivity.kt
@@ -21,16 +21,12 @@ import kotlinx.android.synthetic.main.content_home.*
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
-
-        val fragmentAdapter = TabsPager(supportFragmentManager)
-        viewpager.adapter=fragmentAdapter
-        tab_layout.setupWithViewPager(viewpager)
-
-        //initComponents()
+        initTab()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -41,34 +37,34 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        toolbar.setNavigationIcon(R.drawable.ic_account_circle_black_36dp)
+        toolbar.setTitleTextColor(R.color.primary_dark_material_dark)
 
         nav_view.setNavigationItemSelectedListener(this)
-
     }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.home, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
+    //    override fun onBackPressed() {
+//        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+//            drawer_layout.closeDrawer(GravityCompat.START)
+//        } else {
+//            super.onBackPressed()
+//        }
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.home, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        when (item.itemId) {
+//            R.id.action_settings -> return true
+//            else -> return super.onOptionsItemSelected(item)
+//        }
+//    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
@@ -97,17 +93,23 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    fun initComponents():Unit{
+    fun initTab():Unit{
 
-        var tabLayout: TabLayout = findViewById(R.id.tab_layout)
-        var viewPager: ViewPager = findViewById(R.id.viewpager)
-        var tabsPager  = TabsPager(supportFragmentManager)
+        val fragmentAdapter = TabsPager(supportFragmentManager)
+        viewpager.adapter=fragmentAdapter
+        tab_layout.setupWithViewPager(viewpager)
+        setupTab()
+    }
 
+    fun setupTab(){
+        //SetupTab01
+        tab_layout.getTabAt(0)!!.setIcon(R.drawable.ic_timeline_black_24dp).setText(null)
 
-        viewPager.adapter(tabsPager)
-        tabLayout.setupWithViewPager(viewPager)
+        //SetupTab02
+        tab_layout.getTabAt(1)!!.setIcon(R.drawable.ic_warning_black_24dp).setText(null)
 
-
+        //SetupTab03
+        tab_layout.getTabAt(2)!!.setIcon(R.drawable.ic_search_black_24dp).setText(null)
     }
 
 
--- app/src/main/java/com/example/vinicius/condominium/app/HomeFragment.kt
+++ app/src/main/java/com/example/vinicius/condominium/app/HomeFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.vinicius.condominium.R
+import kotlinx.android.synthetic.main.app_bar_home.*
 
 class HomeFragment: Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home_fragment,container,false)
     }
+
 }
