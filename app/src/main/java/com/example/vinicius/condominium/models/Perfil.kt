4481e368c4dd3740016783d97e23c17c5394ca54
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-07-17 15:31:13 -0300

    Criação dos models a  receber dados da API

---
+++ app/src/main/java/com/example/vinicius/condominium/models/Perfil.kt
@@ -0,0 +1,11 @@
+package com.example.vinicius.condominium.models
+
+class Perfil (
+    var usuario: Usuario,
+    var sexo: String,
+    var telefone: String
+    ) {
+
+
+
+}
---
+++ app/src/main/java/com/example/vinicius/condominium/models/Post.kt
@@ -0,0 +1,10 @@
+package com.example.vinicius.condominium.models
+
+class Post(
+    var descricao: String,
+    var data: String,
+    var tipo: String,
+    var informante: Perfil
+    ){
+
+}
