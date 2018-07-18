b8fe147d23e00a700cb61125656f2fb3b5280cae
hipolito.junior <hipolitocavalcantejr@gmail.com>
2018-07-18 10:09:31 -0300

    Atualizados atributos do modelo

--- app/src/main/java/com/example/vinicius/condominium/models/Perfil.kt
+++ app/src/main/java/com/example/vinicius/condominium/models/Perfil.kt
@@ -2,10 +2,10 @@ package com.example.vinicius.condominium.models
 
 class Perfil (
     var usuario: Usuario,
+    var nome: String,
     var sexo: String,
     var telefone: String
     ) {
-
 
 
 }
