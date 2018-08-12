338df2de21cb952a1525b30b74f32078f005052e
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-08-12 20:29:08 -0300

    Criação do modelo para Visitante

---
+++ app/src/main/java/com/example/vinicius/condominium/models/Visitante.kt
@@ -0,0 +1,14 @@
+package com.example.vinicius.condominium.models
+
+import com.google.gson.annotations.SerializedName
+
+class Visitante(
+    @SerializedName("id") var id: Long,
+    @SerializedName("nome") var nome: String,
+    @SerializedName("sexo") var sexo: String,
+    @SerializedName("telefone") var telefone: String,
+    @SerializedName("data_nascimento") var dataNascimento: String,
+    @SerializedName("morador") var morador: Perfil
+
+    ){
+}
